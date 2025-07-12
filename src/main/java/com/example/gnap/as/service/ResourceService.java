ada0062c10d4d55e2fffe5cf6466938f2662177a
isaac <isaac@gmail.com>
2025-07-12 16:33:18 -0600

    Finalizing repositories and services for Interaction and Resource

--- src/main/java/com/example/gnap/as/service/ResourceService.java
+++ src/main/java/com/example/gnap/as/service/ResourceService.java
@@ -9,6 +9,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Service
+@SuppressWarnings("unused")
 public class ResourceService {
     private final ResourceRepository resourceRepository;
 
